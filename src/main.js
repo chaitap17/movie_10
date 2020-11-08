c814e0b8637488580b8ec9c42d6629874827c8fe
chaitap17 <68525409+chaitap17@users.noreply.github.com>
2020-11-09 00:00:09 +0700

    n

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
+
 import App from "./App.vue";
+import MovieList from "./components/MovieList.vue";
+import CreateMovie from "./components/CreateMovie.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    name: "MovieList",
+    path: "/Movie_list",
+    component: MovieList
+  },
+  {
+    name: "CreateMovie",
+    path: "/create_Movie",
+    component: CreateMovie
+  }
+];
+
+const router = new VueRouter({ mode: "history", routes: routes });
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount("#app");
